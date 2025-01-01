e2da92992a06f88b2e23975326e04bc696b27525
Nathan Jessurun <ntjessu@gmail.com>
2025-01-01 11:41:52 -0600

    `mongodb` forces you to explicitly specify abstract bases. (#7)
    See https://www.tutorialspoint.com/mongoengine/mongoengine_document_inheritance.htm for details

--- backend/adumbra/database/mongo_shim.py
+++ backend/adumbra/database/mongo_shim.py
@@ -2,4 +2,7 @@ from mongoengine import DynamicDocument, QuerySet
 
 
 class ShimmedDynamicDocument(DynamicDocument):
+    """Provides type hinting on `objects`, nothing more."""
+
+    meta = {"abstract": True}
     objects: QuerySet
