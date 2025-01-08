5f0e0d90ce1cf09613bdd70e7d4f0757f831e7b6
Nathan Jessurun <ntjessu@gmail.com>
2025-01-08 17:16:25 -0600

    wip: initial skeleton for assistant page

---
+++ client/src/views/Assistants.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div style="padding-top: 55px" />
+    <div
+      class="album py-5 bg-light"
+      style="overflow: auto; height: calc(100vh - 55px)"
+    >
+      <div class="container">
+        <h2 class="text-center">
+          Assistants
+          <!-- <i
+            class="fa fa-question-circle help-icon"
+            data-bs-toggle="modal"
+            data-bs-target="#helpCategories"
+            aria-hidden="true"
+          /> -->
+        </h2>
+
+        <div class="row justify-content-md-center">
+          <div
+            class="col-md-auto btn-group"
+            role="group"
+            style="padding-bottom: 20px"
+          >
+            <button
+              type="button"
+              class="btn btn-success"
+              data-bs-toggle="modal"
+              data-bs-target="#createCategories"
+            >
+              Upload
+            </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="updatePage()"
+            >
+              Refresh
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CardBase from "@/components/cards/CardBase.vue";
+</script>
